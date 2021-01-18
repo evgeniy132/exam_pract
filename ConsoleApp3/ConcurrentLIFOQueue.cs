6dee4a87ac919fabcfb986eb6f6abd0d7d78225b
evgeniy132 <evgeniy132@gmail.com>
2021-01-18 13:59:43 +0200

    exam

---
+++ ConsoleApp3/ConcurrentLIFOQueue.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    public class ConcurrentLIFOQueue<T>
+    {
+        private T[] _array;
+        private int _length;
+
+        public ConcurrentLIFOQueue(int size)
+        {
+            _array = new T[size];
+            _length = 0;
+        }
+
+        public int Count()
+        {
+            return _length;
+        }
+        public int Length
+        {
+            get
+            {
+                return _array.Length;
+            }
+        }
+
+        public bool TryDequeue(out T result)
+        {
+            lock (_array)
+            {
+                if (Length > 0)
+                {
+                    var index = _array.Length - _length;
+                    result = _array[index];
+                    _array[index] = default(T);
+                    _length--;
+                    return true;
+                }
+                else
+                {
+                    result = default(T);
+                    return false;
+                }
+            }
+        }
+
+        public void Enqueue(T item)
+        {
+            lock (_array)
+            {
+                if (_length < _array.Length)
+                {
+                    _array[_array.Length - _length - 1] = item;
+                    _length++;
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var queue = new ConcurrentLIFOQueue<int>(10);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Task.Run(() => EnqueueAndTryDequeue(queue));
+            }
+
+
+
+            Task.Run(() => Dequeue(queue));
+            Task.Run(() => Dequeue(queue));
+
+            Thread.Sleep(3000);
+
+            Console.ReadLine();
+        }
+        private static void EnqueueAndTryDequeue(ConcurrentLIFOQueue<int> queue)
+        {
+            int result;
+            for (int i = 0; i < queue.Length; i++)
+            {
+                queue.Enqueue(i);
+                queue.TryDequeue(out result);
+            }
+            Console.WriteLine("finish");
+        }
+
+        private static void Dequeue(ConcurrentLIFOQueue<int> queue)
+        {
+            bool result_bool = true;
+            int result;
+            while (result_bool)
+            {
+                if (queue.Count() > 0)
+                {
+                    result_bool = queue.TryDequeue(out result);
+                }
+                else
+                {
+                    result_bool = false;
+                }
+            }
+
+            Console.WriteLine("Done");
+        }
+    }
+}
